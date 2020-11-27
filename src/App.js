eeb2914bb57e6e3698ebb38b1e264089308bbb40
royweb <xytank@gmail.com>
2020-11-27 14:37:17 -0500

    Add navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="text-center">Blockchain Wallet</h1>
+        <nav className="navbar navbar-dark fixed-top bg-primary flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="/"
+          >
+            BC Wallet
+          </a>
+        </nav>
+        <h1 className="text-center mt-5">Blockchain Wallet</h1>
       </div>
     );
   }
