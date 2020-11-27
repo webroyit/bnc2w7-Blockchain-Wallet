aefdbf2eb34ee931c30b056319303d8ac70c3fa7
royweb <xytank@gmail.com>
2020-11-27 13:14:23 -0500

    Update migration file to deploy contract and call mint()

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,15 @@
 const Migrations = artifacts.require("Migrations");
+const FakeDaiToken = artifacts.require("FakeDaiToken");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(FakeDaiToken);
+
+  const tokenMock = await FakeDaiToken.deployed();
+
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0xD6bCe59fd0b6CFE88A22b46D3074703cB11595D2',
+    '1000000000000000000000'
+  );
 };
