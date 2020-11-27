6dcb568bb393e29869a03db391b47fa134563f4f
royweb <xytank@gmail.com>
2020-11-27 13:52:19 -0500

    Install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Blockchain Wallet</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">Blockchain Wallet</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
