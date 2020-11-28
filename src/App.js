3e2c9d4d3e1c4889a354dc81cc049ddf97cd9011
royweb <xytank@gmail.com>
2020-11-28 12:50:50 -0500

    Load the blockchain data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 
 import './App.css';
+import FakeDaiToken from './abis/FakeDaiToken.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: '',
+      fakeDaiToken: null,
+      balance: 0,
+      transactions: []
+    }
+  }
+
   async componentWillMount() {
     await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
   async loadWeb3() {
@@ -19,6 +31,20 @@ class App extends Component {
     else {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+
+    const daiTokenAddress = "0xD58980c457e82eD9ab6AcEfA59C02fD1079BC7Fb" // Replace DAI Address Here
+    const fakeDaiToken = new web3.eth.Contract(FakeDaiToken.abi, daiTokenAddress);
+    this.setState({ fakeDaiToken: fakeDaiToken });
+
+    const balance = await fakeDaiToken.methods.balanceOf(this.state.account).call();
+    this.setState({ balance: web3.utils.fromWei(balance.toString(), 'Ether') });
   }
 
   render() {
